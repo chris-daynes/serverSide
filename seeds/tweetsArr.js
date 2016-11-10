766a9578d3426b73387795be2686657270c36c9a
Brady Gillies <zimsteve@live.com>
2016-11-11 08:46:57 +1300

    small tweet to test git push

--- seeds/tweetsArr.js
+++ seeds/tweetsArr.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('tweets').insert({id: 1, date: 'Mon Sep 24 03:35:21 +0000 2012', city: 'LA, CA', text: 'Aggressive Ponytail #freebandnames', subject: ''}),
-        knex('tweets').insert({id: 2, date: '', city: '', text: '', subject: ''}),
+        knex('tweets').insert({id: 2, date: '', city: '', text: 'Trump is an orange muppet', subject: ''}),
         knex('tweets').insert({id: 3, date: '', city: '', text: '', subject: ''})
       ]);
     });
